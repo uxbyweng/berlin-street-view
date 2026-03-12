0358c26c13b9e0ceff0f3856c68e6bba6571941c
Karsten Weng <karsten@weng.eu>
2026-03-12 10:54:48 +0100

    create header component and implement it in app-shell

--- components/layout/app-shell.tsx
+++ components/layout/app-shell.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import Image from "next/image";
+import Header from "@/components/layout/header";
 
 type AppShellProps = {
   children: React.ReactNode;
@@ -8,32 +7,7 @@ type AppShellProps = {
 export function AppShell({ children }: AppShellProps) {
   return (
     <div className="min-h-screen bg-background text-foreground">
-      <header className="border-b">
-        <div className="mx-auto flex min-h-14 max-w-5xl items-center justify-between px-4">
-          <Link href="/" aria-label="Go to Homepage">
-            <Image
-              src="/images/logo-streetlens-light.png"
-              width={132}
-              height={39}
-              alt="Logo"
-              priority
-            />
-          </Link>
-
-          <nav className="flex items-center gap-4 text-sm">
-            <Link href="/" className="hover:underline hidden md:block">
-              Home
-            </Link>
-            <Link href="/artworks" className="hover:underline">
-              Artworks
-            </Link>
-            <Link href="/artworks/new" className="hover:underline">
-              Add Artwork
-            </Link>
-          </nav>
-        </div>
-      </header>
-
+      <Header />
       <main className="mx-auto w-full max-w-5xl px-4 py-6">{children}</main>
     </div>
   );
