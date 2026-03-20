cdda86cb4cbd9fb50d7801a2a057627f685c41b8
Karsten Weng <karsten@weng.eu>
2026-03-20 13:11:05 +0100

    remove unnessessary comment

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -19,8 +19,6 @@ export default async function HomePage() {
         }}
       />
 
-      {/* <TestToastButton /> */}
-
       <section className="mx-auto mt-8 max-w-6xl">
         <h2 className="text-lg font-semibold mb-4">Recently added ...</h2>
         <ArtworkList artworks={latestArtworks} />
